bbd718e59930e4380c26a6fd7103e64b61f38ccc
nan123456 <2431027810@qq.com>
2019-01-07 22:56:20 +0800

    检验模块

---
+++ src/examine.php
@@ -0,0 +1,39 @@
+<?php
+	require ("../conn.php");
+	header("Access-Control-Allow-Origin: *");
+	// 允许任意域名发起的跨域请求
+	$ret_data = '';
+	$flag = isset($_POST["flag"]) ? $_POST["flag"] : '';
+	if($flag == "Select"){
+		$sql = "select Wmodid,station,name from test where isfinish = '1'";
+		$res=$conn->query($sql);
+		if($res->num_rows>0){
+			$i = 0;
+			while($row=$res->fetch_assoc()){
+				$ret_data["data"][$i]["number"] = $row["Wmodid"];
+				$ret_data["data"][$i]["partName"] = $row["name"];
+				$ret_data["data"][$i]["processName"] = $row["station"];
+				$i++;
+			}
+			$ret_data["success"] = 'success';
+		}
+	}else{
+		$state = $_POST["state"];
+		$sql = "select Wmodid,station,name,utime from test where isfinish = '".$state."'";
+		$res=$conn->query($sql);
+		if($res->num_rows>0){
+			$i = 0;
+			while($row=$res->fetch_assoc()){
+				$ret_data["data"][$i]["number"] = $row["Wmodid"];
+				$ret_data["data"][$i]["partName"] = $row["name"];
+				$ret_data["data"][$i]["processName"] = $row["station"];
+				$ret_data["data"][$i]["checkDate"] = $row["utime"];
+				$i++;
+			}
+			$ret_data["success"] = 'success';
+		}
+	}
+	$conn->close();
+	$json = json_encode($ret_data);
+	echo $json;
+?>
