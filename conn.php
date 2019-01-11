e19ea724c9e77d53e5f10dca7cc208622199933b
nan123456 <2431027810@qq.com>
2019-01-11 20:00:38 +0800

    2019年1月11日20:00:35

--- conn.php
+++ conn.php
@@ -1,14 +1,14 @@
 <?php	
     header("Access-Control-Allow-Origin:*");
 //连接数据库
-	$servername = "localhost:3306";
-	$username = "root";
-	$password = "123456";
+	$servername = "192.168.0.136:3306";
+	$username = "admin";
+	$password = "root";
 	$dbname = "jmmes";	
 	$conn = new mysqli($servername, $username, $password, $dbname);	
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}else{
-		//echo "Connected successfully";
+//		echo "Connected successfully";
 	}	
 ?> 
