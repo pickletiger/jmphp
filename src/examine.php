a1ebdcf2bcced1607ef81a9ff6f34b7a1bec8b27
nan123456 <2431027810@qq.com>
2019-01-20 16:02:50 +0800

    检验图片路径

--- src/examine.php
+++ src/examine.php
@@ -30,7 +30,7 @@
 		$ret_data["success"] = 'success';
 	}else{
 		$state = $_POST["state"];
-		$sql = "select Wmodid,station,name,utime from test where isfinish = '".$state."'";
+		$sql = "select Wmodid,station,name,utime,photourl from test where isfinish = '".$state."'";
 		$res=$conn->query($sql);
 		if($res->num_rows>0){
 			$i = 0;
@@ -39,6 +39,7 @@
 				$ret_data["data"][$i]["partName"] = $row["name"];
 				$ret_data["data"][$i]["processName"] = $row["station"];
 				$ret_data["data"][$i]["checkDate"] = $row["utime"];
+				$ret_data["data"][$i]["photourl"] = $row["photourl"];
 				$i++;
 			}
 			$ret_data["success"] = 'success';
