822228908e78d254283829405de3879fb0b6fa89
nan123456 <2431027810@qq.com>
2019-03-13 11:06:04 +0800

    电子看板和系统首页

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,21 +1,38 @@
 <?php
 	require("../conn.php");
 	header("Access-Control-Allow-Origin: *"); // 允许任意域名发起的跨域请求
-	$name=$_POST["name"];
-	$utime = time();
-	$account=$_POST["account"];
-	$postword=sha1($_POST["postword"]);
-	$sql = "select account from user where account='$account'";
-	$res=$conn->query($sql);
-	if($res->num_rows>0){
-		echo "该用户已经存在";
-	}else{
-		$sql1 = "INSERT INTO `user`(account,password,name,ctime) VALUES('$account','$postword','$name','$utime')";
-		$res1=$conn->query($sql1);
-		if($res1->num_rows>0){
-			echo "添加用户成功";
-		}else{
-			echo "添加失败";
+	
+	$flag = isset($_POST["flag"])?$_POST["flag"]:'';
+	switch($flag){
+		case 'Select':
+		$account = isset($_POST["account"])?$_POST["account"]:'';
+		$sql = "SELECT account,name FROM user WHERE account = '".$account."'";
+		$res=$conn->query($sql);
+		if($res->num_rows>0){
+			$ret_data["success"] = 'success';
+			while($row=$res->fetch_assoc()){
+				$ret_data["account"] = $row["account"];
+				$ret_data["name"] = $row["name"];
+			}
+		}else {
+			$ret_data["success"] = 'error';
 		}
-	}
+		break;
+		case 'Save':
+			$name=$_POST["name"];
+			$utime = time();
+			$account=$_POST["account"];
+			$postword=sha1($_POST["postword"]);
+			$sql = "UPDATE `user` SET `name` = '".$name."', `password` = '".$postword."' , utime = '".$utime."' WHERE account = '".$account."'";
+			$res=$conn->query($sql);
+			if($res==true){
+				$ret_data["success"] = 'success';
+			}else {
+				$ret_data["success"] = 'error';
+			}
+			break;
+		}
+	$conn->close();
+	$json = json_encode($ret_data);
+	echo $json;
 ?>
--- src/electronic/WorkshopTask.php
+++ src/electronic/WorkshopTask.php
@@ -2,10 +2,10 @@
  	require("../../conn.php");
 	header("Access-Control-Allow-Origin: *"); // 允许任意域名发起的跨域请求
  	$sqldata='';
- 	$sql="SELECT * from `workshop_k` where isfinish = '2'";
+ 	$sql="SELECT * from `workshop_k` where isfinish = '2' ORDER BY id DESC limit 10";
  	$result = $conn->query($sql);
  	while ($row = $result->fetch_assoc()) {
-		$sqldata=$sqldata.'{
+		$sqldata=$sqldata.'{ 
 			"Serial":"'.$row["route"].'",
 			"name":"'.$row["name"].'",
 			"Group":"'.$row["station"].'",
