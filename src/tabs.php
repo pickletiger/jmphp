ec9b1883b02bf9eda66e2f0ae7384b56181be048
Lin971018 <30288831+Lin971018@users.noreply.github.com>
2019-03-15 18:34:32 +0800

    Revert "Revert "2018.3.15-18点32分""
    This reverts commit a9e8356e79dcd935996d768bdeb19e8e7208b5d9.

--- src/tabs.php
+++ src/tabs.php
@@ -6,44 +6,53 @@
 
 	if($flag == "Unread"){
 		$department = $_POST["department"]; 
-		$sql = "SELECT content,time,id FROM message WHERE state='0' AND department='".$department."' ";
+		$sql = "SELECT content,time,id,station,workstate,route FROM message WHERE state='0' AND department='".$department."' ";
 		$res=$conn->query($sql);
 		if($res->num_rows>0){
 			$i = 0;
 			while($row=$res->fetch_assoc()){
 				
-				$ret_data["data"][$i]["title"] = $row["content"];
+				$ret_data["data"][$i]["address"] = $row["content"];
 				$ret_data["data"][$i]["date"] = $row["time"];
 				$ret_data["data"][$i]["id"] = $row["id"];
+				$ret_data["data"][$i]["tag"] = $row["station"];
+				$ret_data["data"][$i]["state"] = $row["workstate"];
+				$ret_data["data"][$i]["route"] = $row["route"];
 				$i++;
 			}
 			$ret_data["success"] = 'success';
 		}
 	}else if($flag == "Read"){
 		$department = $_POST["department"]; 
-		$sql = "SELECT content,time,id FROM message WHERE state='1' AND department='".$department."' ";
+		$sql = "SELECT content,time,id,station,workstate,route FROM message WHERE state='1' AND department='".$department."' ";
 		$res=$conn->query($sql);
 		if($res->num_rows>0){
 			$i = 0;
 			while($row=$res->fetch_assoc()){
 				
-				$ret_data["data"][$i]["title"] = $row["content"];
+				$ret_data["data"][$i]["address"] = $row["content"];
 				$ret_data["data"][$i]["date"] = $row["time"];
 				$ret_data["data"][$i]["id"] = $row["id"];
+				$ret_data["data"][$i]["tag"] = $row["station"];
+				$ret_data["data"][$i]["state"] = $row["workstate"];
+				$ret_data["data"][$i]["route"] = $row["route"];
 				$i++;
 			}
 			$ret_data["success"] = 'success';
 		}
 	}else if($flag == "Recycle"){
-		$sql = "SELECT content,time,id FROM message where state='1' or state='0' ";
+		$sql = "SELECT content,time,id,station,workstate,route FROM message where state='1' or state='0' ";
 		$res=$conn->query($sql);
 		if($res->num_rows>0){
 			$i = 0;
 			while($row=$res->fetch_assoc()){
 				
-				$ret_data["data"][$i]["title"] = $row["content"];
+				$ret_data["data"][$i]["address"] = $row["content"];
 				$ret_data["data"][$i]["date"] = $row["time"];
 				$ret_data["data"][$i]["id"] = $row["id"];
+				$ret_data["data"][$i]["tag"] = $row["station"];
+				$ret_data["data"][$i]["state"] = $row["workstate"];
+				$ret_data["data"][$i]["route"] = $row["route"];
 				$i++;
 			}
 			$ret_data["success"] = 'success';
