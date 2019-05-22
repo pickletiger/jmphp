93346de81c832218a99d758646c205c8d9977a57
nan123456 <2431027810@qq.com>
2019-05-22 16:36:45 +0800

    分车间电子看板

--- src/electronic/WorkshopTask.php
+++ src/electronic/WorkshopTask.php
@@ -2,7 +2,8 @@
  	require("../../conn.php");
 	header("Access-Control-Allow-Origin: *"); // 允许任意域名发起的跨域请求
  	$sqldata='';
- 	$sql="SELECT * from `workshop_k` where isfinish = '2' ORDER BY id DESC limit 10";
+	$arr = $_POST["arr"];
+ 	$sql="SELECT * from `workshop_k` where isfinish = '2' and route in $arr ORDER BY id DESC limit 10";
  	$result = $conn->query($sql);
  	while ($row = $result->fetch_assoc()) {
 		$sqldata=$sqldata.'{ 
